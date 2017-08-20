f0436c7b9c7253ed6bc0e94e06233a09de8fe1be
xy.kong <xy.kong@gmail.com>
2017-08-20 19:44:43 +0800

    Fix bug Error: TextEditor disposed     at t. (file:///Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.main.js:28:3140811)     at t.e.handle (file:///Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.main.js:28:3100447)     at s (file:///Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.main.js:28:1029376)     at f (file:///Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/workbench.main.js:28:1030031)     at _combinedTickCallback (internal/process/next_tick.js:67:7)     at process._tickCallback (internal/process/next_tick.js:98:9) Add Progress status.

--- extension.js
+++ extension.js
@@ -18,6 +18,7 @@ function activate(context) {
     var disposable = vscode.commands.registerCommand('extension.formatAll', function () {
         // The code you place here will be executed every time your command is executed
 
+
         if (!!arguments && arguments.length > 0 && !!arguments[0].path) {
             var selectedFolder = arguments[0].path;
             if (fs.existsSync(selectedFolder) && fs.lstatSync(selectedFolder).isDirectory()) {
@@ -26,10 +27,28 @@ function activate(context) {
                 var files = vscode.workspace.findFiles(pattern);
 
                 files.then((value) => {
-                    vscode.window.showInformationMessage('Files selected: ' + value.length);
-                    openAndFormat(value, 0);
+                    // vscode.window.showInformationMessage('Files selected: ' + value.length);
+
+                    vscode.window.withProgress({ location: vscode.ProgressLocation.Window, title: 'formating' }, p => {
+                        return new Promise((resolve, reject) => {
+
+                            openAndFormat(value, 0, p, resolve);
+
+                            // p.report({ message: 'Start working...' });
+                            // let count = 0;
+                            // let handle = setInterval(() => {
+                            //     count++;
+                            //     p.report({ message: 'Worked ' + count + ' steps' });
+                            //     if (count >= 10) {
+                            //         clearInterval(handle);
+                            //         resolve();
+                            //     }
+                            // }, resolve);
+                        });
+                    });
+
                 }, (reason) => {
-                    console.log(reason);
+                    console.log("findFiles: " + reason);
                 });
             }
         }
@@ -47,19 +66,26 @@ function deactivate() {
 }
 exports.deactivate = deactivate;
 
-function openAndFormat(files, index) {
+function openAndFormat(files, index, p, resolve) {
 
     if (files.length <= index) {
+        resolve();
         return;
     }
 
+    var path = files[index].path.replace(vscode.workspace.rootPath, '');
+    p.report({ message: `formating: ${path}` });
+
     console.log("format file:" + files[index].path);
     vscode.workspace.openTextDocument(files[index].path).then(doc => {
-        vscode.window.showTextDocument(doc);
-        vscode.commands.executeCommand('csharpfixformat.process').then(() => {
-            openAndFormat(files, index + 1);
-        }, (reason) => {
-            console.log(reason);
+        vscode.window.showTextDocument(doc).then(() => {
+
+            vscode.commands.executeCommand('csharpfixformat.process').then(() => {
+                openAndFormat(files, index + 1, p, resolve);
+            }, (reason) => {
+                console.error("executeCommand failed: ", reason);
+                vscode.window.showWarningMessage(path + `, Format failed: ${reason}. You can close all documents then try agagin.`)
+            });
         });
     });
 }
